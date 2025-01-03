1f28ed7854be2a2f6ebe8ba9abba15c73d636e6c
ahsan <ahsan123.bilal@gmail.com>
2025-01-03 18:02:25 +0500

    form complete

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,13 +1,20 @@
 import 'package:ahsan_dev/utils/app_resources.dart';
+import 'package:ahsan_dev/widgets/contact_me_button.dart';
+import 'package:ahsan_dev/widgets/expandable_textfield.dart';
 import 'package:ahsan_dev/widgets/footer.dart';
 import 'package:ahsan_dev/widgets/header.dart';
 import 'package:ahsan_dev/widgets/hover_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   ContactPage({super.key});
 
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
   final emailController = TextEditingController();
   final emailFocus = FocusNode();
   final messageController = TextEditingController();
@@ -36,11 +43,9 @@ class ContactPage extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment,
                 children: [
-                  const SizedBox(
-                    width: 200,
-                  ),
+                  const SizedBox(width: 200), // Adjusted padding on the left
+
                   // Social Media Icons Section
                   const Expanded(
                     flex: 1,
@@ -48,100 +53,92 @@ class ContactPage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         HoverIcon(
-                          icon: FontAwesomeIcons.linkedinIn,
-                          label: 'LinkedIn',
-                        ),
+                            icon: FontAwesomeIcons.linkedinIn,
+                            label: 'LinkedIn'),
                         SizedBox(height: 15),
                         HoverIcon(
-                          icon: FontAwesomeIcons.github,
-                          label: 'GitHub',
-                        ),
+                            icon: FontAwesomeIcons.github, label: 'GitHub'),
                         SizedBox(height: 15),
                         HoverIcon(
-                          icon: FontAwesomeIcons.medium,
-                          label: 'Medium',
-                        ),
+                            icon: FontAwesomeIcons.medium, label: 'Medium'),
                         SizedBox(height: 15),
                         HoverIcon(
-                          icon: FontAwesomeIcons.stackOverflow,
-                          label: 'Stack Overflow',
-                        ),
+                            icon: FontAwesomeIcons.stackOverflow,
+                            label: 'Stack Overflow'),
                       ],
                     ),
                   ),
+
                   const SizedBox(width: 50),
+
                   // Contact Form Section
-                  Expanded(
-                    flex: 2,
-                    child: Form(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TextFormField(
-                            controller: emailController,
-                            focusNode: emailFocus,
-                            textInputAction: TextInputAction.next,
-                            maxLines: 1,
-                            decoration: AppResources.decoration.fieldDecoration(
-                              context: context,
-                              hintText: "Email Address",
-                            ),
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          TextFormField(
-                            controller: subjectController,
-                            focusNode: subjectFocus,
-                            textInputAction: TextInputAction.next,
-                            maxLines: 1,
-                            decoration: AppResources.decoration.fieldDecoration(
-                              context: context,
-                              hintText: "Subject",
-                            ),
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 20),
-                          TextFormField(
-                            controller: messageController,
-                            focusNode: messageFocus,
-                            maxLines: 4,
-                            decoration: AppResources.decoration.fieldDecoration(
-                              context: context,
-                              hintText: "Message",
-                            ),
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 30),
-                          ElevatedButton(
-                            onPressed: () {
-                              // Add form submission logic here
-                            },
-                            style: ElevatedButton.styleFrom(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 32, vertical: 16),
-                              backgroundColor: Colors.blue,
-                            ),
-                            child: const Text(
-                              'Send Message',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                  Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Form(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Email Field
+                              TextFormField(
+                                controller: emailController,
+                                focusNode: emailFocus,
+                                textInputAction: TextInputAction.next,
+                                maxLines: 1,
+                                decoration:
+                                    AppResources.decoration.fieldDecoration(
+                                  context: context,
+                                  hintText: "Email Address",
+                                ),
+                                style: const TextStyle(fontSize: 16),
                               ),
-                            ),
+                              const SizedBox(height: 20),
+
+                              // Subject Field
+                              TextFormField(
+                                controller: subjectController,
+                                focusNode: subjectFocus,
+                                textInputAction: TextInputAction.next,
+                                maxLines: 1,
+                                decoration:
+                                    AppResources.decoration.fieldDecoration(
+                                  context: context,
+                                  hintText: "Subject",
+                                ),
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              const SizedBox(height: 20),
+
+                              // Message Field with Expandable TextField
+                              ExpandableTextField(
+                                height: 100,
+                                maxHeight: 300,
+                                dividerHeight: 20,
+                                dividerSpace: 2,
+                                key: const Key('message'),
+                                child: TextFormField(
+                                  controller: messageController,
+                                  focusNode: messageFocus,
+                                  maxLines: null,
+                                  expands: true,
+                                  decoration:
+                                      AppResources.decoration.fieldDecoration(
+                                    context: context,
+                                    hintText: "Message",
+                                  ),
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                              ),
+
+                              // Row to keep the button next to the fields
+                            ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                  const SizedBox(
-                    width: 50,
-                  ),
+                  const SizedBox(width: 50),
                 ],
               ),
             ),
--- lib/utils/app_decoration.dart
+++ lib/utils/app_decoration.dart
@@ -67,25 +67,37 @@ class AppDecoration {
       filled: true,
       fillColor:
           fillColor ?? const Color(0xffF1F4FE), // White or light background
-      contentPadding: const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+      contentPadding: const EdgeInsets.symmetric(
+          vertical: 16, horizontal: 20), // Padding to control text alignment
+      alignLabelWithHint: true, // Aligns the hint text at the top
       enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(radius ?? 5)),
+        borderSide: const BorderSide(
+          // Thin border, will be invisible when not focused
+          color:
+              Colors.transparent, // Border color transparent when not focused
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(radius ?? 5)),
+        borderSide: const BorderSide(
+          width: 2, // Thicker border when focused
+          color: Color(0xffCCD7FD), // Border color on focus
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
         borderRadius: BorderRadius.all(Radius.circular(radius ?? 5)),
         borderSide: const BorderSide(
           width: 2,
           color: Color(0xffCCD7FD),
         ),
       ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(radius ?? 10)),
-        borderSide: const BorderSide(width: 2, color: Color(0xffCCD7FD)),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(radius ?? 10)),
-        borderSide: const BorderSide(width: 2, color: Color(0xffCCD7FD)),
-      ),
       focusedErrorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.all(Radius.circular(radius ?? 10)),
-        borderSide: const BorderSide(width: 2, color: Color(0xffCCD7FD)),
+        borderRadius: BorderRadius.all(Radius.circular(radius ?? 5)),
+        borderSide: const BorderSide(
+          width: 2,
+          color: Color(0xffCCD7FD),
+        ),
       ),
     );
   }
